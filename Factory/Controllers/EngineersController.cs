ebf16770b117688e7d8ec53b8f8f65d13967dbcc
Scott O'Neil <scottieoneil@gmail.com>
2021-03-19 10:33:48 -0700

    add index / create engineers

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -15,5 +15,32 @@ namespace Factory.Controllers
       _db = db;
     }
 
+    public ActionResult Index()
+    {
+      List<Engineer> model = _db.Engineers.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Engineer eng)
+    {
+      _db.Engineers.Add(eng);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var eng = _db.Engineers
+          .Include(e => e.JoinEntities)
+          .ThenInclude(j => j.Machine)
+          .FirstOrDefault(e => e.EngineerId == id);
+      return View(eng);
+    }
   }
 }
